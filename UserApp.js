bd3b56b913cbedd52374d3feef0e0a7d8f6327ad
svk091 <150667192+svk091@users.noreply.github.com>
2023-12-27 21:53:20 +0530

    Create UserApp.js

---
+++ UserApp.js
@@ -0,0 +1,134 @@
+/*          ASSINGNMENT
+    connect to MongoDB
+1./signup    -> (username,password) add credentials to DB
+2./signin    -> by jwt athuntication
+3./users    -> return users
+*/
+
+const express = require('express');
+const mongoose = require('mongoose');
+const z = require('zod');
+const jwt = require('jsonwebtoken')
+
+const app = express();
+const secretKey =  "EveryDayIsNotSunday"; 
+
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://vamsikrishna:krishnamongodb@krdb.notjpqi.mongodb.net/UserApp');
+
+const userSchema = mongoose.Schema({
+    email : String,
+    password: String
+});
+
+const Users = mongoose.model('Users', userSchema);
+
+async function checkIfUserExists(emailToCheck) {
+    const isExists = await Users.findOne({
+        email : emailToCheck
+    });
+    return !!isExists;
+}
+
+async function checkIfUserExists(emailToCheck, passwordToCheck) {
+    const isExists = await Users.findOne({
+        email : emailToCheck,
+        password : passwordToCheck
+    });
+    return !!isExists;
+}
+
+const credentialSchema = z.object({
+    email : z.string().email(),
+    password : z.string().min(8).regex(/[a-zA-Z]/).regex(/\d/)
+})
+
+app.post('/signup', async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const validateCredentials = credentialSchema.safeParse({
+        email : email,
+        password : password
+    })
+
+    if(!validateCredentials.success) {
+        return res.json({
+            msg : 'Invalid input. Please correct and try again'
+        })
+    }
+
+    const userExists = await checkIfUserExists(email);
+    if(userExists) {
+        return res.json({
+            msg : 'The email address is already in use'
+        })
+    }else{
+        const user = new Users({
+            email : email,
+            password : password
+        });
+        user.save()
+            .then(() => {
+                return res.json({
+                    msg : "Successfully Signedup"
+                })
+            })
+    }
+});
+
+app.post('/signin', async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const userExists = await checkIfUserExists(email, password);
+
+    if(!userExists) {
+        return res.json({
+            msg : "Incorrect Username or Password"
+        })
+    } 
+
+    const payload = { email : email,password : password};
+    
+
+    const token = jwt.sign(payload, secretKey);
+    return res.json({
+        token
+    })
+})
+
+app.get('/users', async (req, res) => {
+    const token = req.headers.authorization;
+    try{
+        if(!token) {
+            return res.json({
+                msg : 'token missing'
+            })
+        }
+
+    try{
+        const decoded = jwt.verify(token, secretKey);
+    }catch(err) {
+        return res.json({
+            msg : 'verifying error'
+        })
+    }
+        
+        const email = decoded.email;
+        
+        const otherUsers = await Users.find({}).toArray();
+        return res.json({otherUsers})
+    }catch(err) {
+        return res.status(403).json({
+            msg : "Error",
+        })
+    }
+    
+
+})
+
+app.listen(3000, () => {
+    console.log('Server Started....')
+});
